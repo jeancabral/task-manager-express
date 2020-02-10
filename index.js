3c344cd53c85b7e682686a22a428ce83d07ccbe9
Jean Cabral <jeancabral@gmail.com>
2020-02-10 15:33:33 -0200

    port fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 //create an express application 
 const app = express();
 
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 //enable json
 app.use(express.json());
